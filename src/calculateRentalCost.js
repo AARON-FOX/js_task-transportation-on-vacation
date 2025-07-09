74bacdda201cdeadef80b07da58709a0b4ef2b70
Artem Vikuliev <ffox2645@gmail.com>
2025-07-09 14:32:48 +0300

    added correct task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,34 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days >= 7) {
-    return 40 * days - 50;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  let mainPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return mainPrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3 && days < 7) {
-    return 40 * days - 20;
+  if (days >= SHORT_TERM) {
+    return mainPrice - SHORT_TERM_DISCOUNT;
   }
 
-  return 40 * days;
+  return mainPrice;
+
+  // My old solution:
+  // if (days >= 7) {
+  //   return 40 * days - 50;
+  // }
+
+  // if (days >= 3 && days < 7) {
+  //   return 40 * days - 20;
+  // }
+
+  // return 40 * days;
 }
 
 module.exports = calculateRentalCost;
